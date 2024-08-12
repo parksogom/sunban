3f3f13b5c99d6e2d15c1561252ef2517ef4b6666
parksogom <gomgomno@naver.com>
2024-08-12 23:18:48 +0900

    Update firebase.js
    보안

--- src/datasources/firebase.js
+++ src/datasources/firebase.js
@@ -2,8 +2,8 @@ import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 
 const db = firebase.initializeApp({
-    apiKey: "AIzaSyDbFw0CsBsLT0HxRIJi0EV2EcsOpMfjEAM",
-    authDomain: "realtime-5b099.firebaseapp.com",
+    apiKey: "",
+    authDomain: "",
 })
 
 export const firebaseAuth = db.auth()
